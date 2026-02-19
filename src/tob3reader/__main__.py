ad24e4f2600308223ee14e61f6081c320ede68e1
mattjbr123 <matbro@ceh.ac.uk>
2026-02-19 17:34:49 +0000

    Added src files back

---
+++ src/tob3reader/__main__.py
@@ -0,0 +1,6 @@
+"""This file is run when `python -m tob3reader` is called. Put your client code here if you have any."""
+
+from tob3reader.module import add_int
+
+print("Hello World!")
+print(f"1 + 1 = {add_int(1, 1)}")
