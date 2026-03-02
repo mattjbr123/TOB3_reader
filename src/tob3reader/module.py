1a6bba167a8a35d7174ebdff42749512bfc17b74
mattjbr123 <matbro@ceh.ac.uk>
2026-03-02 18:33:43 +0000

    Added decoder of FP2 and ULONG types

--- src/tob3reader/module.py
+++ src/tob3reader/module.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import numpy as np
 
 
 def ftc(filepath: str) -> str:
@@ -40,6 +42,7 @@ def read_header(filepath: str) -> (list[str], int, int):
             headerlines.append(line)
         datapos = infile.tell()
         filesize = infile.seek(0, os.SEEK_END)
+    print(headerlines)
 
     return headerlines, datapos, filesize
 
@@ -57,6 +60,28 @@ def read_body(filepath: str) -> list:
         body = alllines[nhlines:]
 
     return body
+
+
+def decode_fp2(data: bytes) -> float:
+    """Decode a Campbell Scientific FP2 two-byte float."""
+
+    print("Decoding FP2")
+    raw = int.from_bytes(data, byteorder="big")
+
+    sign = (raw >> 15) & 0x01  # bit 15
+    exponent = (raw >> 13) & 0x03  # bits 14-13
+    mantissa = (raw >> 0) & 0x1FFF  # bits 12-0
+
+    value = mantissa * (10**-exponent)
+    if sign:
+        value = -value
+    return value
+
+
+def decode_ulong(data: bytes) -> int:
+    """Deode a ULONG (unsigned, 4-byte) little-endian integer"""
+    print("Decoding ULONG")
+    return int.from_bytes(data, byteorder="little")
 
 
 def read_tob1(filepath: str):
@@ -79,14 +104,42 @@ def read_tob1(filepath: str):
     for datatype in datatypes:
         print(datatype)
         if datatype == "ULONG":
-            bytelen = 8
+            bytelen = 4
             linelength += bytelen
             bytelens.append(bytelen)
         elif datatype == "FP2":
             bytelen = 2
             linelength += bytelen
             bytelens.append(bytelen)
+        else:
+            raise TypeError("Unsupported datatype: " + datatype)
     nlines = (filesize - datapos) / linelength
+    assert nlines % 1 == 0  # check it's a whole number
+    nlines = int(nlines)
+    data = np.zeros((nlines, ncols))
+
+    # read in the data element by element
+    # first put all the binary date into memory
+    with open(filepath, "rb") as infile:
+        allfile = infile.read()
+    # remove the header
+    data = allfile[datapos:]
+    startbyte = 0
+    for line in range(nlines):
+        for b in range(len(bytelens)):
+            bytelen = bytelens[b]
+            datatype = datatypes[b]
+            # read the next x bytes and put it in the empty data array
+            element = data[startbyte : startbyte + bytelen]
+            startbyte += bytelen
+            print(element)
+            if datatype == "ULONG":
+                value = decode_ulong(element)
+            elif datatype == "FP2":
+                value = decode_fp2(element)
+            print(value)
+        if line == 1:
+            sys.exit()
 
     return bytelens, linelength, nlines
 
