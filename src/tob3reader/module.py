e8117ed70f6b14b63f9100ef121e339bc8f4da6d
mattjbr123 <matbro@ceh.ac.uk>
2026-03-04 16:53:01 +0000

    Finished TOB1 file functions

--- src/tob3reader/module.py
+++ src/tob3reader/module.py
@@ -2,6 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+import datetime as dt
 
 
 def ftc(filepath: str) -> str:
@@ -39,7 +40,7 @@ def read_header(filepath: str) -> (list[str], int, int):
     with open(filepath, "rb") as infile:
         headerlines = []
         for nline in range(nhlines):
-            line = str(infile.readline()).strip("b").strip("'")
+            line = str(infile.readline()).strip("b").strip("'").strip("\\r\\n")
             headerlines.append(line)
         datapos = infile.tell()
         filesize = infile.seek(0, os.SEEK_END)
@@ -88,7 +89,7 @@ def decode_fp4(data: bytes) -> float:
     exponent = (raw >> 24) & 0x7F  # bits 30-24
     mantissa = (raw >> 0) & 0xFFFFFF  # bits 23-0
 
-    value = mantissa * (2**exponent)  ## EXPONENT MAY BE NEGATIVE. DOCS UNCLEAR
+    value = mantissa * (2**exponent)
     if sign:
         value = -value
     return value
@@ -104,14 +105,26 @@ def decode_long(data: bytes) -> int:
     return int.from_bytes(data, byteorder="little", signed=True)
 
 
-def decode_time(data: int):
+def decode_time(data: pd.DataFrame, origin: dt.datetime = dt.datetime(1990, 1, 1)) -> pd.DataFrame:
     """Translate the time information from the TOB1 or TOB3 files into
     something human readable"""
-    pass
-    return
+    if "SECONDS" and "NANOSECONDS" in list(data.columns):
+        data.index = pd.to_datetime(
+            data.loc[:, "SECONDS"] + data.loc[:, "NANOSECONDS"] / 1e9,
+            unit="s",
+            origin=origin,
+        )
+    else:
+        raise IndexError(
+            "At least one of SECONDS and NANOSECONDS not "
+            + "present in data. Cannot establish unique time coords."
+            + "Data headers are: "
+            + str(list(data.columns))
+        )
+    return data
 
 
-def read_tob1(filepath: str):
+def read_tob1(filepath: str) -> pd.DataFrame:
     """Reads in TOB1 file and outputs as TOA5 ASCII
 
     :param filepath: path to the input TOB1 datafile
@@ -123,8 +136,10 @@ def read_tob1(filepath: str):
     """
 
     headerlines, datapos, filesize = read_header(filepath)
-    datatypes = headerlines[-1][:-4].split(",")
+    datatypes = headerlines[-1].split(",")
     datatypes = [datatype.strip('"') for datatype in datatypes]
+    names = headerlines[1].split(",")
+    names = [name.strip('"') for name in names]
     ncols = len(datatypes)
     linelength = 0
     bytelens = []
@@ -174,6 +189,12 @@ def read_tob1(filepath: str):
                 raise TypeError("Unsupported datatype: " + datatype)
             datastore[line, b] = value
     datastore = pd.DataFrame(datastore)
+    datastore.columns = names
+    datastore = decode_time(datastore)
+    # datastore = process_nans(datastore)
+
+    splits = split30(datastore)
+    to_files(splits, headers=headerlines[:-1])
 
     return datastore
 
@@ -190,11 +211,63 @@ def read_tob3(filepath: str):
     """
 
 
-def split30(filepath_toa5: str):
-    """Splits a TOA5 file output by read_tob1 or read_tob3 into 30min chunks
+def split30(data: pd.DataFrame) -> list[pd.DataFrame]:
+    """Splits a pd dataframe output by read_tob1 or read_tob3 into 30min chunks
 
-    :param filepath_toa5: path to input TOA5 file
+    :param data: pd.DataFrame containing time-indexed data
     :type filepath_toa5: str
+    :return: DESCRIPTION
+    :rtype: TYPE
+
+    """
+    data = data.drop(columns=["SECONDS", "NANOSECONDS", "RECORD"], errors="ignore")
+    firstfirst = data.index[0].to_pydatetime()
+    last = data.index[-1].to_pydatetime()
+    if 29 >= firstfirst.minute >= 0:
+        nextminute = 30
+    elif 59 >= firstfirst.minute >= 30:
+        nextminute = 60
+    minstonext30 = dt.timedelta(minutes=nextminute - firstfirst.minute)
+    next30 = firstfirst + minstonext30
+    next30 = dt.datetime(next30.year, next30.month, next30.day, next30.hour, next30.minute, 0, 0) - dt.timedelta(
+        microseconds=1
+    )
+
+    split_datasets = []
+    first = firstfirst
+    while first < last:
+        split_data = data.loc[first:next30, :]
+        if not split_data.empty:
+            split_datasets.append(split_data)
+        first = next30
+        next30 += dt.timedelta(minutes=30)
+
+    return split_datasets
+
+
+def to_files(splits: list[pd.DataFrame], headers: list[str], outdir: str = ".") -> None:
+    """
+    Outputs each dataset in splits to separate, time-stamped files in TOA5
+    ASCII format
+    """
+    sitename = headers[0].split(",")[1].strip('"')
+    for ds in splits:
+        timestamp = ds.index[0].to_pydatetime().strftime("%Y_%m_%d_%H%M")
+        outpath = os.path.join(outdir, "TOA5_" + sitename + "_FluxRaw_" + timestamp + ".dat")
+        with open(outpath, mode="w") as outfile:
+            for line in headers:
+                outfile.write(line + "\r\n")
+        ds.to_csv(outpath, index=None, header=None, float_format="%.2f", mode="a")  # change nan format to "NAN"
+
+    return
+
+
+def process_nans(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Replace invalid values with NaNs
+
+    :param data: DESCRIPTION
+    :type data: pd.DataFrame
     :return: DESCRIPTION
     :rtype: TYPE
 
